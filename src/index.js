a4b050d48a13d0b8d3cf027f6ffb497ab45f3343
Natalia Milovanova <hameleonka28@gmail.com>
2021-10-22 09:07:17 -0400

    builds for prod

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import server from './services/server';
 server();
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
